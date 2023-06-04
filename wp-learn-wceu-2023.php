95d55be53172d027dbcec7ad4088a90338443bd1
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-06-04 16:49:29 +0200

    blockProps and RichText

--- wp-learn-wceu-2023.php
+++ wp-learn-wceu-2023.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Learn WCEU 2023
  * Plugin Description: A plugin to learn how to develop blocks using vanilla JavaScript.
  * Plugin URI: https://learn.wordpress.org
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Jonathan Bossenger
  * Author URI: https://jonathanbossenger.com
  */
